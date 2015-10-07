1cb14aa2d252558ab74b3e56a5407437eda577d3
gen141430851 <gen14143085@COBC33365.CityBathColl.ac.uk>
2015-10-07 11:43:37 +0100

    2

--- MyApplication/app/src/main/java/com/example/gen14143085/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/gen14143085/myapplication/MainActivity.java
@@ -2,16 +2,130 @@ package com.example.gen14143085.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnTrue;
+    private Button btnFalse;
+    private TextView lblQuestion;
+    private ImageView imgPicture;
+
+    private List<QuestionObject> questions;
+
+    private QuestionObject currentQuestion;
+    private int index;
+    private int score;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnTrue = (Button)findViewById(R.id.btnTrue);
+        btnFalse = (Button)findViewById(R.id.btnFalse);
+        lblQuestion = (TextView)findViewById(R.id.lblQuestion);
+        imgPicture = (ImageView)findViewById(R.id.imgPicture);
+
+        lblQuestion.setText("Is London the capital of England?");
+        imgPicture.setImageResource(R.drawable.englandflag);
+
+        index = 0;
+        score = 0;
+
+        //onclick listeners
+        btnFalse.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                determineButtonPress(false);
+            }
+        });
+
+        btnTrue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                determineButtonPress(true);
+            }
+        });
     }
+    generateQuestions();
+    setUpQuestion();
+    }
+
+   private void generateQuestions(){
+
+       questions = new ArrayList<>();
+
+       questions.add(new QuestionObject("Is the capital of France Paris?", true, R.drawable.franceflag));
+       questions.add(new QuestionObject("Is the capital of Egypt Giza?", false, R.drawable.egyptflag));
+       questions.add(new QuestionObject("Is the capital of Czech Republic Prague?", true, R.drawable.czechflag));
+       questions.add(new QuestionObject("Is the capital of Argentina Córdoba?", false, R.drawable.argentinaflag));
+       questions.add(new QuestionObject("Is the capital of Liechtenstein Vaduz?", true, R.drawable.liechtensteinflag));
+       questions.add(new QuestionObject("Is the capital of Iceland Reykjavík?", true, R.drawable.icelandflag));
+       questions.add(new QuestionObject("Is the capital of Indonesia Jakarta?", true, R.drawable.indonesiaflag));
+       questions.add(new QuestionObject("Is the capital of Cameroon Douala?", false, R.drawable.cameroonflag));
+       questions.add(new QuestionObject("Is the capital of Uzbekistan Tashkent?", true, R.drawable.uzbekistanflag));
+
+
+    }
+
+    private void setUpQuestion(){
+
+        if (index == questions.size()) {
+            Log.d("DANIELLE_APP", "ended all the questions");
+        }
+
+        else {
+            currentQuestion = questions.get(index);
+
+            lblQuestion.setText(currentQuestion.getQuestion());
+            imgPicture.setImageResource(currentQuestion.getPicture());
+
+            index++;
+        }
+    }
+
+
+    private void determineButtonPress(boolean answer){
+        boolean expectedAnswer = currentQuestion.isAnswer();
+
+        if (answer == expectedAnswer){
+            Toast.makeText(MainActivity.this, "Correct!!", Toast.LENGTH_SHORT).show();
+            score++;
+        }
+
+        else {
+            Toast.makeText(MainActivity.this, "Wrong!!", Toast.LENGTH_SHORT).show()
+        }
+
+        setUpQuestion();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ MyApplication/app/src/main/java/com/example/gen14143085/myapplication/QuestionObject.java
@@ -0,0 +1,29 @@
+package com.example.gen14143085.myapplication;
+
+/**
+ * Created by gen14143085 on 07/10/2015.
+ */
+public class QuestionObject {
+
+    private String question;
+    private boolean answer;
+    private int picture;
+
+    public QuestionObject(String question, boolean answer, int picture) {
+        this.question = question;
+        this.answer = answer;
+        this.picture = picture;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public boolean isAnswer() {
+        return answer;
+    }
+
+    public int getPicture() {
+        return picture;
+    }
+}
